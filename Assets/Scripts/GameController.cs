ed501714170768722650254ca4593ee1fe05bcf7
Unknown <kamil.imielowski@gmail.com>
2019-02-06 19:24:42 +0100

    UI design

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GUIText ectsText;
+    private int ects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ects = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddEcts(int newEctsValue)
+    {
+        ects += newEctsValue;
+        UpdateEcts();
+    }
+
+    void UpdateEcts()
+    {
+        ectsText.text = "Score: " + ects;
+    }
+}
